b6369bd9569299c9e056ad46e25769398551ee88
AndKrogh <anders.krogh99@gmail.com>
2024-02-01 10:58:52 +0100

    setting up structure of project

---
+++ Calendar-app/src/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import '../Styling/index.css'
+
+function App() {
+  const [nav, setNav] = useState(0);
+  const [clicked, setClicked] = useState();
+  const [e, v] = useState();
+
+  return (
+    <div className="App">
+      <p>hej</p>
+    </div>
+  )
+}
+
+export default App
