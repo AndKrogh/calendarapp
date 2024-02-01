a541f9aac43a18078e30dadbb4f0a4ade2148333
AndKrogh <anders.krogh99@gmail.com>
2024-02-01 13:41:51 +0100

    creating calendar template

--- Calendar-app/src/App/App.jsx
+++ Calendar-app/src/App/App.jsx
@@ -1,15 +1,50 @@
-import { useState } from 'react'
-import '../Styling/index.css'
+import { useState, useEffect } from 'react';
+import { CalendarHeader } from '../CalendarHeader/CalendarHeader';
+import { Day } from  '../CalendarHeader/CalendarHeader';
 
 function App() {
   const [nav, setNav] = useState(0);
   const [clicked, setClicked] = useState();
-  const [e, v] = useState();
+  const [days, setDays] = useState([]);
+  const [dateDisplay, setDateDisplay] = useState('');
+  
+  const [events, setEvents] = useState(localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : []);
+
+  const eventForDate = date => events.find(e => e.date === date );
+
+  useEffect(() => {
+    localStorage.setItem('events', JSON.stringify(events));
+  }, [events]);
 
   return (
-    <div className="App">
-      <p>hej</p>
-    </div>
+    <>
+      <div id="container">
+        <CalendarHeader/>
+        <div id="weekdays">
+          <div>Sunday</div>
+          <div>Monday</div>
+          <div>Tuesday</div>
+          <div>Wednesday</div>
+          <div>Thursday</div>
+          <div>Friday</div>
+          <div>Saturday</div>
+        </div>
+
+        <div id="calendar">
+          {days.map((d,index) => {
+            <Day
+              key = {index}
+              day = {d}
+              onClick = {() => {
+                if(d.value !== 'padding') {
+                  setClicked(d.date);
+                }
+              }}
+            />
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ Calendar-app/src/CalendarHeader/CalendarHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const CalendarHeader = () => {
+return( <>
+    <div id="header">
+        <div id="monthDisplay"></div>
+        <div>
+          <button id="backButton">Back</button>
+          <button id="nextButton">Next</button>
+        </div>
+    </div>
+</>);
+};
