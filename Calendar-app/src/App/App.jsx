26c257b2bd2878789e0b14e7d1517832fb0ab18a
AndKrogh <anders.krogh99@gmail.com>
2024-02-07 12:06:01 +0100

    Making day boxes logic

--- Calendar-app/src/App/App.jsx
+++ Calendar-app/src/App/App.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { CalendarHeader } from '../CalendarHeader/CalendarHeader';
-import { Day } from  '../CalendarHeader/CalendarHeader';
+import { Day } from  '../Day/Day';
 
 function App() {
   const [nav, setNav] = useState(0);
-  const [clicked, setClicked] = useState();
   const [days, setDays] = useState([]);
   const [dateDisplay, setDateDisplay] = useState('');
+  const [clicked, setClicked] = useState();
   
   const [events, setEvents] = useState(localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : []);
 
@@ -16,8 +16,34 @@ function App() {
     localStorage.setItem('events', JSON.stringify(events));
   }, [events]);
 
+  useEffect(()=> {
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dt = new date();
+
+    if (nav !== 0) {
+      dt.setMonth(new Date().getMonth() + nav)
+    }
+
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear();
+
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+    const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    });
+
+    setDateDisplay(`${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+
+  }, [events, nav]);
+
   return (
-    <>
       <div id="container">
         <CalendarHeader/>
         <div id="weekdays">
@@ -36,16 +62,15 @@ function App() {
               key = {index}
               day = {d}
               onClick = {() => {
-                if(d.value !== 'padding') {
-                  setClicked(d.date);
+                if(day.value !== 'padding') {
+                  setClicked(day.date);
                 }
               }}
             />
           })}
         </div>
       </div>
-    </>
   )
 }
 
-export default App
+export default App;
